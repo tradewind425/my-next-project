9583bebd8f0ffadfe4108e7814996ae66c9a38b9
とらで <tradewind425@gmail.com>
2024-04-02 20:50:17 +0900

    復帰ポイント

--- back/express/src/index.ts
+++ back/express/src/index.ts
@@ -7,7 +7,22 @@ const app = express();
 const port = process.env.PORT || 3000; // .envファイルからPORTを読み込む、デフォルトは3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!');
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Hello World</title>
+    </head>
+    <body>
+      <h1>Hello World!!!!</h1>
+      <script>
+        console.log('Hello World!!!!');
+      </script>
+    </body>
+    </html>
+  `);
 });
 
 app.listen(port, () => {
