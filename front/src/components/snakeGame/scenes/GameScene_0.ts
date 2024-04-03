998a0a1f46385f2173e9057cd6ea2212978f8500
とらで <tradewind425@gmail.com>
2024-04-03 14:31:53 +0900

    ヘビの描画

--- front/src/components/snakeGame/scenes/GameScene_0.ts
+++ front/src/components/snakeGame/scenes/GameScene_0.ts
@@ -1,23 +1,18 @@
 import Phaser from 'phaser';
-// Ball_0 のインポートに変更
-import { Ball_0 } from '../models/GameModel';
 
 export class GameScene_0 extends Phaser.Scene {
-    // standardBall を Ball_0 型に変更
-    private ball_0!: Ball_0;
+    // 蛇の頭の表現用のテキスト
+    private snakeHead!: Phaser.GameObjects.Text;
 
     constructor() {
         super({ key: 'GameScene_0' });
     }
 
     preload(): void {
-        // ここで必要なアセットをロード
+        // 必要なアセットをロードする場所
     }
 
     create(): void {
-        // Arcade Physics を使用するため、matter.world.setBounds() の呼び出しを削除またはコメントアウト
-        // 代わりに、必要に応じて Arcade Physics の設定をここに追加
-
         // ESCキーを押すとポーズ画面を表示
         this.input.keyboard.on('keydown-ESC', () => {
             this.scene.pause();
@@ -28,12 +23,15 @@ export class GameScene_0 extends Phaser.Scene {
     }
 
     private setupGame(): void {
-        // ボールのインスタンス化を Ball_0 に変更
-        this.ball_0 = new Ball_0(this, 400, 300);
+        // 画面中央に蛇の頭を表現するテキストオブジェクトを配置
+        const { width, height } = this.sys.game.canvas;
+        this.snakeHead = this.add.text(width / 2, height / 2, '◆', {
+            font: '48px Arial',
+            color: '#00ff00'
+        }).setOrigin(0.5);
     }
 
     update(): void {
-        // ボールの更新処理を ball_0 に対して実行
-        this.ball_0.update();
+        // 蛇の頭の更新処理をここに実装する
     }
 }
